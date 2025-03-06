234b46334cb4018ace016d8e53e7252b999da57d
Mistoo2002 <surazsanju@gmail.com>
2025-03-06 21:44:13 +0545

    Updating App Code Design

--- lib/main.dart
+++ lib/main.dart
@@ -2,40 +2,49 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text(
-            'My First App',
-         style: TextStyle(
+          'My First App',
+          style: TextStyle(
             color: Colors.white,
-        ),
+          ),
         ),
         centerTitle: true,
         backgroundColor: Colors.deepPurple[600],
       ),
       body: Center(
         child: Text(
-            'Hello Ninjas',
+          'Hello Ninjas !!!',
           style: TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
             letterSpacing: 2,
             color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Colors.deepPurple[600], // Deep purple background
-        child: Text(
-          'Click',
-          style: TextStyle(
-            color: Colors.white, // White text color
-          ),
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
+
 
 
